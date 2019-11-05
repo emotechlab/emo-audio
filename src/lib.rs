c730b46c86f2c7bf69b1261e3a9fe9c38db904b9
Daniel McKenna <daniel@emotech.co>
2019-11-05 16:01:34 +0000

    Cargo fmt run

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 /// around ndarray.
 use ndarray::Array2;
 use rustfft::num_complex::Complex;
-use std::f32::{NAN, consts::PI};
+use std::f32::{consts::PI, NAN};
 
 /// Module containing an implementation of a short time fourier transform
 pub mod stft;
