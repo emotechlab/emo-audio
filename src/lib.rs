74ff0b0af28399d5365eaa4bde926ff56745016c
Daniel McKenna <daniel@emotech.co>
2019-11-05 14:50:48 +0000

    Added phase spectra impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 /// around ndarray.
 use ndarray::Array2;
 use rustfft::num_complex::Complex;
+use std::f32::consts::{NAN, PI};
 
 /// Module containing an implementation of a short time fourier transform
 pub mod stft;
@@ -15,11 +16,23 @@ pub trait FrequencyComponents {
 }
 
 impl FrequencyComponents for Array2<Complex<f32>> {
+    /// Convert the complex array into it's magnitude spectra
     fn mag(&self) -> Array2<f32> {
         self.mapv(|x| (x.re.powi(2) + x.im.powi(2)).sqrt())
     }
 
+    /// Convert every complex number into it's phase. Result may contain NAN
+    /// values for instances where `Im(x) == 0 && Re(x) == 0`
     fn phase(&self) -> Array2<f32> {
-        unimplemented!()
+        self.mapv(|x| {
+            if x.re > 0.0 || x.im.abs() > std::f32::EPSILON {
+                let mag = (x.re.powi(2) + x.im.powi(2)).sqrt();
+                2.0 * (x.im / (mag + x.re)).atan()
+            } else if x.re < 0.0 && x.im.abs() <= std::f32::EPSILON {
+                PI
+            } else {
+                NAN
+            }
+        })
     }
 }
